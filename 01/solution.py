662d93fa7259b01daa6fa32294807b7a81a34a84
Mattia Ridolfo <mattia.ridolfo@adevinta.com>
2025-12-04 14:37:06 +0100

    first puzzle! second part attempt

--- 01/solution.py
+++ 01/solution.py
@@ -12,7 +12,7 @@ def read_input():
         lines = [line.strip() for line in lines]
     return lines
 
-DIAL_START = 50
+DIAL_START = 98
 TURNING_POINT = 100
 
 def move_dial(current_position, direction, steps):
@@ -30,7 +30,35 @@ def move_dial(current_position, direction, steps):
             current_position = current_position + steps
     return current_position
 
-def main():
+def move_dial_and_count_passes(current_position, direction, steps):
+    passes = steps // TURNING_POINT
+
+    if steps > TURNING_POINT:
+        steps = steps % TURNING_POINT
+        
+    if direction == 'L':
+        if current_position - steps < 0:
+            if current_position != 0:
+                passes += 1
+            current_position = current_position - steps + TURNING_POINT
+            
+        else:
+            current_position = current_position - steps
+    elif direction == 'R':
+        if current_position + steps >= TURNING_POINT:
+            if current_position != 0:
+                passes += 1
+            current_position = current_position + steps - TURNING_POINT
+            
+        else:
+            current_position = current_position + steps
+
+    if current_position == 0:
+        passes += 1
+    
+    return current_position, passes
+
+def part_1():
     rotations = read_input()
     
     dial_position = DIAL_START
@@ -46,5 +74,22 @@ def main():
 
     print(f"Dial position was zero {dial_position_zero_cnt} times")
 
+def part_2():
+    rotations = read_input()
+    # rotations = ['L101', 'L75', 'R200', 'L125']  # Example rotations for the second half
+    
+    dial_position = DIAL_START
+    print(f"Starting position: {dial_position}")
+
+    dial_position_zero_cnt = 0
+
+    for rotation in rotations:
+        dial_position, passes = move_dial_and_count_passes(dial_position, rotation[0], int(rotation[1:]))
+        dial_position_zero_cnt += passes
+        print(f"Moved {rotation}, new position: {dial_position}, passes through zero: {dial_position_zero_cnt}")
+
+    print(f"Dial position was zero {dial_position_zero_cnt} times")
+
+
 if __name__ == "__main__":
-    main()
+    part_2()

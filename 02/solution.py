89cc93f06c3b3bae193c5f5dd603ad9bf3067be5
Mattia Ridolfo <mattia.ridolfo@adevinta.com>
2025-12-04 21:50:29 +0100

    day 2 - second part

--- 02/solution.py
+++ 02/solution.py
@@ -29,17 +29,14 @@ def is_repeating_twice(num):
 
 def is_repeating_n_times(num, n):
     num_len = len(num)
-    if num_len % 2 == 0:
-        half = num_len // 2
-        first_half = num[:half]
-        second_half = num[half:]
-        if first_half == second_half:
+    if num_len % n == 0:
+        segments = [num[i: i + n] for i in range(0, num_len, n)]
+        if all(segment == segments[0] for segment in segments):
             print(f"Found matching number: {num}")
-            repeating_numbers.append(num)
+            return True
+    return False
 
-if __name__ == "__main__":
-    sequences = read_input()
-
+def part_1(sequences):
     repeating_numbers = []
 
     for seq in sequences:
@@ -47,6 +44,27 @@ if __name__ == "__main__":
             is_rep = is_repeating_twice(num)
             if is_rep:
                 repeating_numbers.append(num)
-    
-    print(f"Total repeating numbers found: {len(repeating_numbers)}")
+    print(f"Total repeating numbers twice found: {len(repeating_numbers)}")
     print(f'sum: {sum(int(num) for num in repeating_numbers)}')
+
+def part_2(sequences):
+    repeating_numbers = []
+
+    for seq in sequences:
+        for num in parse_sequence(seq):
+            i = 1
+            while i <= len(num) // 2:
+                is_rep = is_repeating_n_times(num, i)
+                if is_rep:
+                    repeating_numbers.append(num)
+                    break
+                i += 1
+    print(f"Total repeating numbers any times found: {len(repeating_numbers)}")
+    print(f'sum: {sum(int(num) for num in repeating_numbers)}')    
+
+if __name__ == "__main__":
+    sequences = read_input()
+    print("Part 1:")
+    part_1(sequences)
+    print("\nPart 2:")
+    part_2(sequences)
